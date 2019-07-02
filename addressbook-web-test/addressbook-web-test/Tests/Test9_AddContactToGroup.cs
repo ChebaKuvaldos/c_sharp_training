653aa42b32eefddda89cb00b37bef1f01775678a
Vsevolod <cheba.kuvaldos@gmail.com>
2019-07-02 23:08:34 +0300

    Task17.2_second chance. Completion of tests for removal\adding contact in groups

--- addressbook-web-test/addressbook-web-test/Tests/Test9_AddContactToGroup.cs
+++ addressbook-web-test/addressbook-web-test/Tests/Test9_AddContactToGroup.cs
@@ -9,32 +9,49 @@ using OpenQA.Selenium;
 
 namespace addressbook_web_test
 {
+    [TestFixture]
     public class Test9_AddContactToGroup : AuthTestBase
     {
         [Test]
         public void AddContactToGroup()
         {
+            Class3_ContactData contact;
+            app.Contacts.ContactExist();
+            app.Groups.GroupExist();
+
             Class2_GroupData group = Class2_GroupData.GetAllGroupInfo()[0];
             List<Class3_ContactData> oldList = group.GetContacts();
-            Class3_ContactData contact = Class3_ContactData.GetAllContactInfo()
+
+            if (Class2_GroupData.GetAllGroupInfo().Count == oldList.Count)
+            {
+                contact = new Class3_ContactData("new", "contact");
+                app.Contacts.CreateContact(contact);
+            }
+
+            contact = Class3_ContactData.GetAllContactInfo()
                                             .Except(oldList).First();
+
             app.Contacts.AddContactToGroup(contact, group);
+
             List<Class3_ContactData> newList = group.GetContacts();
             oldList.Add(contact);
             oldList.Sort();
             newList.Sort();
             Assert.AreEqual(oldList, newList);
         }
+
         [Test]
         public void RemoveContactToGroup()
         {
-            app.Contacts.ContactExist();
             Class3_ContactData contact;
+
+            app.Contacts.ContactExist();
+            app.Groups.GroupExist();
+            
             Class2_GroupData group = Class2_GroupData.GetAllGroupInfo()
                                     .Find(x => x.Name.Contains("NewTest"));
             if (group == null)
             {
-                app.Groups.GroupExist();
                 group = Class2_GroupData.GetAllGroupInfo()[0];
             }
 
